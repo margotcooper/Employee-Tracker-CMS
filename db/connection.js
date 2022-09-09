8b770fec65a580ae2859add6ecee99616fb32825
Margot Cooper Gruen <margotgruen@gmail.com>
2022-09-08 18:36:51 -0700

    folder structure

---
+++ db/connection.js
@@ -0,0 +1,2 @@
+//connect to mysql backend
+//copy and paste another connection.js minus database name using.
---
+++ index.js
@@ -0,0 +1,3 @@
+//include inquirer to generate prompts for users
+//all of command line questions and makes queries to backend
+//can split query in a class in a separate js file (not necessary)
